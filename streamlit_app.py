f3ca9e6e59763450c5dd4a9cc6522c977d8e39bb
YopiYei <edtc.torresuni@gmail.com>
2026-09-01 09:37:38 -0500

    Initial Streamlit project structure

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Engineering Tools",
+    page_icon="🧮",
+    layout="wide",
+)
+
+st.title("Engineering Tools")
+
+st.write(
+    """
+    Colección de herramientas de ingeniería y métodos numéricos.
+    
+    Este proyecto irá creciendo con calculadoras, métodos numéricos,
+    mecánica de medios continuos, concreto, geotecnia y otras herramientas.
+    """
+)
